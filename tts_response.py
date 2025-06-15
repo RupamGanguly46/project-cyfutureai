4b96b61a7fb575a213e59464d99994a5be5fe723
RupamGanguly46 <rupam.ganguly46@gmail.com>
2025-06-15 14:12:38 +0530

    pyttsx3 replaced by Google TTS to support speaking other languages, langdetect used to inform gTTS which lang is the response in; tree.txt updated with chatbot.log; main.py is now set to run using uvicorn command on terminal rather than running python script, and logger works better as now it supports multiple languages using utf-8 and manual loggers; lifespan code used to log the start and stop.

--- tts_response.py
+++ tts_response.py
@@ -1,17 +1,35 @@
-import pyttsx3
+# import pyttsx3
+# import uuid
+# import os
+
+# OUTPUT_DIR = "static/audio"
+# os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+# engine = pyttsx3.init()
+
+# def generate_tts(text):
+#     audio_id = f"response_{uuid.uuid4().hex}.mp3"
+#     path = os.path.join(OUTPUT_DIR, audio_id)
+
+#     engine.save_to_file(text, path)
+#     engine.runAndWait()
+
+#     return f"/static/audio/{audio_id}"
+
+from gtts import gTTS
+from langdetect import detect
 import uuid
 import os
 
 OUTPUT_DIR = "static/audio"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-engine = pyttsx3.init()
-
 def generate_tts(text):
+    lang = detect(text)
+    tts = gTTS(text=text, lang=lang)
+
     audio_id = f"response_{uuid.uuid4().hex}.mp3"
     path = os.path.join(OUTPUT_DIR, audio_id)
 
-    engine.save_to_file(text, path)
-    engine.runAndWait()
-
+    tts.save(f"{path}")
     return f"/static/audio/{audio_id}"
