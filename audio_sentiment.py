0c204664688d56593cf53e4c6465fd9b8f69a164
RupamGanguly46 <rupam.ganguly46@gmail.com>
2025-06-16 09:48:10 +0530

    audio/tone sentiment analysis with improved accuracy and noticeable difference, using meta's pretrained wav2vec2 model for audio classfication replacing pyAudiAnalysis

--- audio_sentiment.py
+++ audio_sentiment.py
@@ -1,9 +1,26 @@
-from pyAudioAnalysis import audioSegmentation as aS
+# from pyAudioAnalysis import audioSegmentation as aS
+
+# def analyze_audio_emotion(audio_path):
+#     try:
+#         flags, _, _ = aS.mtFileClassification(audio_path, "pyAudioAnalysis/data/svmSpeechEmotion", "svm")
+#         mapping = {0: "neutral", 1: "happy", 2: "sad", 3: "angry"}
+#         return mapping.get(flags[0], "neutral")
+#     except:
+#         return "neutral"
+
+from transformers import pipeline
+dictio = {"ang":"angry", "hap":"happy", "neu":"neutral", "sad":"sad"}
+emotion_pipeline = pipeline(task="audio-classification", model="superb/wav2vec2-base-superb-er")
 
 def analyze_audio_emotion(audio_path):
-    try:
-        flags, _, _ = aS.mtFileClassification(audio_path, "pyAudioAnalysis/data/svmSpeechEmotion", "svm")
-        mapping = {0: "neutral", 1: "happy", 2: "sad", 3: "angry"}
-        return mapping.get(flags[0], "neutral")
-    except:
-        return "neutral"
+
+    result = emotion_pipeline(f"{audio_path}")
+    # result: list of dicts like [{'score': 0.99, 'label': 'ang'}, ...]
+
+    sorted_scores = sorted(result, key=lambda x: x['score'], reverse=True)
+    top = sorted_scores[0]
+    if top['label'] == 'neu' and len(sorted_scores) > 1:
+        # If next best is close (within 10%), prefer it over neutral
+        if sorted_scores[1]['score'] >= 0.9 * top['score']:
+            return sorted_scores[1]['label']
+    return dictio[top['label']]
